817c2a707380ab13a4a54487e5f787ccae7f1cc3
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-04-10 15:39:28 +0200

    feat: add search by gender

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import movieList from './data/movie'
+
 
 function App() {
 
+  const [movie] = useState(movieList)
+  const [filteredMovie, setFilteredMovie] = useState(movie)
+  const [searchCategory, setSearchCategory] = useState('')
+
+  useEffect(() => {
+
+    let updateMovie = movie
+
+    if (searchCategory !== '') {
+      updateMovie = updateMovie.filter(element => element.genre === searchCategory)
+    }
+
+    setFilteredMovie(updateMovie)
+  }, [searchCategory, movie])
 
   return (
     <>
-
+      <h1>Lista Film</h1>
+      <section>
+        <label>Cerca per generi</label>
+        <div>
+          <select value={searchCategory} onChange={e => setSearchCategory(e.target.value)}>
+            <option value="">----</option>
+            <option>Fantascienza</option>
+            <option>Thriller</option>
+            <option>Romantico</option>
+            <option>Azione</option>
+          </select>
+        </div>
+      </section>
+      <section>
+        {filteredMovie.map((element) => <div key={element.id}>
+          <h4>{element.title}</h4>
+          <p>Genere: {element.genre}</p>
+        </div>)}
+      </section>
     </>
   )
 }
